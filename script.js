e9d6f757c34f92b136fbe7f12b43878955347f30
Prince Bhatt <princebhatt316@gmail.com>
2024-04-28 15:22:55 +0530

    Page 1 Done, Testing 2

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/* ****************************************************** Scroll *********************************************************** */
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('#main'),
+  smooth: true,
+});
+
+/* ****************************************************** Scroll *********************************************************** */
+
 /* ****************************************************** Navbar *********************************************************** */
 
 const open = document.querySelector('.container');
@@ -39,55 +48,168 @@ document.addEventListener('DOMContentLoaded', function () {
 
 /* *************************************************** Image Slider ******************************************************** */
 
-let nextDom = document.getElementById('next');
-let prevDom = document.getElementById('prev');
+class Slider {
+  constructor() {
+    this.el = document.querySelector('.js-slider');
+    this.images = [...this.el.querySelectorAll('.js-slide__image')];
+    this.contents = [...this.el.querySelectorAll('.js-slide__content')];
 
-let carouselDom = document.querySelector('.carousel');
-let SliderDom = carouselDom.querySelector('.carousel .list');
-let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
-let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
-let timeDom = document.querySelector('.carousel .time');
+    this.data = {
+      current: 0,
+      last: 0,
+      total: this.images.length - 1,
+    };
 
-thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 3000;
-let timeAutoNext = 5000;
+    this.state = {
+      animating: false,
+    };
 
-nextDom.onclick = function () {
-  showSlider('next');
-};
+    this.next = this.next.bind(this);
 
-prevDom.onclick = function () {
-  showSlider('prev');
-};
-let runTimeOut;
-let runNextAuto = setTimeout(() => {
-  next.click();
-}, timeAutoNext);
-function showSlider(type) {
-  let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
-  let thumbnailItemsDom = document.querySelectorAll(
-    '.carousel .thumbnail .item'
-  );
-
-  if (type === 'next') {
-    SliderDom.appendChild(SliderItemsDom[0]);
-    thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-    carouselDom.classList.add('next');
-  } else {
-    SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-    thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-    carouselDom.classList.add('prev');
+    this.init();
+    this.startAutoplay();
   }
-  clearTimeout(runTimeOut);
-  runTimeOut = setTimeout(() => {
-    carouselDom.classList.remove('next');
-    carouselDom.classList.remove('prev');
-  }, timeRunning);
 
-  clearTimeout(runNextAuto);
-  runNextAuto = setTimeout(() => {
-    next.click();
-  }, timeAutoNext);
+  changeSlide() {
+    this.state.animating = true;
+
+    const currentImageSlide = this.images[this.data.last];
+    const nextImageSlide = this.images[this.data.current];
+
+    const currentContent = this.contents[this.data.last];
+    const nextContent = this.contents[this.data.current];
+
+    const currentContentItems =
+      currentContent.querySelectorAll('.js-stagger-item');
+    const nextContentItems = nextContent.querySelectorAll('.js-stagger-item');
+
+    const currentImageSlideImg = currentImageSlide.querySelector('img');
+    const nextImageSlideImg = nextImageSlide.querySelector('img');
+
+    const tl = new TimelineLite({
+      paused: true,
+      onComplete: () => {
+        this.state.animating = false;
+      },
+    });
+
+    tl.set([nextImageSlide, nextContent], {
+      autoAlpha: 1,
+      zIndex: 2,
+    })
+      .set([currentImageSlide, currentContent], {
+        zIndex: 1,
+      })
+      .staggerFromTo(
+        currentContentItems,
+        0.75,
+        {
+          y: 0,
+          alpha: 1,
+        },
+        {
+          y: -30,
+          alpha: 0,
+          ease: Expo.easeIn,
+        },
+        0.075,
+        0
+      )
+      .fromTo(
+        currentImageSlide,
+        1.5,
+        {
+          xPercent: 0,
+        },
+        {
+          xPercent: -15,
+          ease: Expo.easeInOut,
+        },
+        0
+      )
+      .fromTo(
+        nextImageSlide,
+        1.5,
+        {
+          xPercent: 100,
+        },
+        {
+          xPercent: 0,
+          ease: Expo.easeInOut,
+        },
+        0
+      )
+      .fromTo(
+        nextImageSlideImg,
+        1.5,
+        {
+          xPercent: -100,
+          scale: 1.2,
+        },
+        {
+          xPercent: 0,
+          scale: 1,
+          ease: Expo.easeInOut,
+        },
+        0
+      )
+      .staggerFromTo(
+        nextContentItems,
+        1,
+        {
+          y: 60,
+          alpha: 0,
+        },
+        {
+          y: 0,
+          alpha: 1,
+          ease: Expo.easeOut,
+        },
+        0.075,
+        0.75
+      )
+      .set([currentImageSlide, currentContent], {
+        autoAlpha: 0,
+      });
+
+    tl.play();
+  }
+
+  next() {
+    if (this.state.animating) return;
+
+    this.data.last = this.data.current;
+    this.data.current =
+      this.data.current === this.data.total ? 0 : this.data.current + 1;
+
+    this.changeSlide();
+  }
+
+  startAutoplay() {
+    this.autoplayInterval = setInterval(() => {
+      this.next();
+    }, 3000);
+  }
+
+  stopAutoplay() {
+    clearInterval(this.autoplayInterval);
+  }
+
+  addListeners() {
+    this.el.addEventListener('click', this.next);
+    this.el.addEventListener('mouseenter', () => {
+      this.stopAutoplay();
+    });
+    this.el.addEventListener('mouseleave', () => {
+      this.startAutoplay();
+    });
+  }
+
+  init() {
+    this.addListeners();
+  }
 }
+
+const slider = new Slider();
 
 /* *************************************************** Image Slider ******************************************************** */
